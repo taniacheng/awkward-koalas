9e6235f8321936301302e1bbe8b3cd73249535a9
Tania Cheng <tania.cheng15@gmail.com>
2017-07-31 05:51:50 +0000

    added navigation

---
+++ includes/navigation.php
@@ -0,0 +1,22 @@
+<header>
+    <nav class="navbar navbar-default">
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+                <span class="sr-only"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="index.php" class="navbar-brand">Hello</a>
+        </div>
+        <!--end navbar-header-->
+        <div class="collapse navbar-collapse" id="main-menu">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="register.php">Sign Up</a></li>
+                <li><a href="login.php">Sign In</a></li>
+            </ul>
+        </div>
+        <!--end collapse-->
+    </nav>
+</header>
