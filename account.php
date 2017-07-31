f31d291d5842a75007785fb46c8d5e26e83dbcf8
Tania Cheng <tania.cheng15@gmail.com>
2017-07-31 06:29:08 +0000

    added user account page

---
+++ account.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+    <?php include("includes/head.php"); ?>
+    
+    <body>
+        <?php include("includes/navigation.php"); ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <form id="account-update" action="account.php" method="post">
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control" id="username" name="username">
+                        </div>
+                        <!--end form group-->
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email">
+                        </div>
+                        <!--end form group-->
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password1" name="password1">
+                        </div>
+                        <!--end form group-->
+                        <div class="form-group">
+                            <label for="password">Re-type Password</label>
+                            <input type="password" class="form-control" id="password2" name="password2">
+                        </div>
+                        <!--end form group-->
+                        <div class="text-center">
+                            <button type="submit" name="submit" class="btn btn-info">Update</button>
+                        </div>
+                    </form>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+    </body>
+</html>
